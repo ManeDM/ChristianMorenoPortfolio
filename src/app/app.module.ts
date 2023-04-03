0c94c819e1fe54d206048e767f4fc1fac78400d5
ManeDM <mane657@hotmail.com>
2023-04-03 12:47:30 -0500

    Portfolio Upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgParticlesModule } from "ng-particles";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BackgroundComponent } from './components/background/background.component';
+import { TopInfoComponent } from './components/top-info/top-info.component';
+import { MainContainerComponent } from './components/main-container/main-container.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    BackgroundComponent,
+    TopInfoComponent,
+    MainContainerComponent,
+    FooterComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgParticlesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
