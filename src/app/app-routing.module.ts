28fc90956d6ff4e6af43bf658b4a6aa7ce5acc34
ManeDM <mane657@hotmail.com>
2023-04-04 15:24:30 -0500

    scrolling &  navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainContainerComponent } from './components/main-container/main-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+
+  @ViewChild('homeSection') homeSection!: ElementRef;
+  @ViewChild('aboutSection') aboutSection!: ElementRef;
+  @ViewChild('tecnologiesSection') tecnologiesSection!: ElementRef;
+  @ViewChild('projectsSection') projectsSection!: ElementRef;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const homeLink = document.querySelector('a[href="#home"]');
+    const aboutLink = document.querySelector('a[href="#about"]');
+    const tecnologiesLink = document.querySelector('a[href="#tecnologies"]');
+    const projectsLink = document.querySelector('a[href="#projects"]');
+
+    homeLink?.addEventListener('click', () => {
+      this.homeSection.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    });
+
+    aboutLink?.addEventListener('click', () => {
+      this.aboutSection.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    });
+
+    tecnologiesLink?.addEventListener('click', () => {
+      this.tecnologiesSection.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    });
+
+    projectsLink?.addEventListener('click', () => {
+      this.projectsSection.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    });
+  }
 
 }
