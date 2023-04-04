0ede8ac178f95c2544270100549135761ddbc417
ManeDM <mane657@hotmail.com>
2023-04-04 16:21:46 -0500

    footer icon navigate to navbar

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+
+  @ViewChild('navSection') navSection!: ElementRef;
+
+  constructor () {}
+ 
+  ngOnInit(): void {
+
+    const navbarLink = document.querySelector('a[href="#navbar"]');
+
+    navbarLink?.addEventListener('click', () => {
+      this.navSection.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    });
+    
+  }
 
 }
